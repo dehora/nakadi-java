c9a5cf47eade4b686d8248a1703f55198d0c6a91
dehora <bill@dehora.net>
2016-11-10 20:08:21 +0000

    Extract common data field for all Event categories.
    In the API, only the DataChangeEvent has a data field; the other two are
    effectively raw JSON objects with no place to bind custom data. However, in
    the absence of a common top level structure for events and their payloads, this
    client gives all three categories a data field and the implementation does
    bespoke serdes with business and undefined categories to match their Open API
    definitions. This allows us to support custom data uniformly while providing
    useful types for undefined, business and data change categories.

--- nakadi-java-client/src/main/java/nakadi/Event.java
+++ nakadi-java-client/src/main/java/nakadi/Event.java
@@ -1,7 +1,23 @@
 package nakadi;
 
 /**
- * Marker interface for known event types.
+ * Interface for known event categories.
+ *
+ * <p>
+ * In the API, only the DataChangeEvent has a data field; the other two are
+ * effectively raw JSON objects with no place to bind custom data. However, in
+ * the absence of a common top level structure for events and their payloads, this
+ * client gives all three categories a data field and the implementation does
+ * bespoke serdes with business and undefined categories to match their Open API
+ * definitions. This allows us to support custom data uniformly while providing
+ * useful types for undefined, business and data change categories.
+ * </p>
  */
-public interface Event {
+public interface Event<T> {
+
+  /**
+   * @return the custom data
+   */
+  T data();
+
 }
