f4a00b05808e8e60beb7c1b87d2f1086b6419a3d
dehora <bill@dehora.net>
2016-11-10 18:27:02 +0000

    Start 0.0.4

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,7 @@
 ### Changes
 
+#### 0.0.4
+
 #### 0.0.3
 
 - Add Javadoc to API. [#19](https://github.com/zalando-incubator/nakadi-java/issues/19)
--- nakadi-java-client/src/main/java/nakadi/Version.java
+++ nakadi-java-client/src/main/java/nakadi/Version.java
@@ -2,5 +2,5 @@ package nakadi;
 
 public class Version {
 
-  public static final String VERSION = "0.0.3";
+  public static final String VERSION = "0.0.4-SNAPSHOT";
 }
