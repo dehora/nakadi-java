658440c5e9eb6bb04cd4b4a33879d03618188843
dehora <bill@dehora.net>
2016-11-13 16:22:14 +0000

    Allow token provider to provide tokens by scope name

--- nakadi-java-client/src/main/java/nakadi/TokenProvider.java
+++ nakadi-java-client/src/main/java/nakadi/TokenProvider.java
@@ -10,10 +10,16 @@ import java.util.Optional;
 @FunctionalInterface
 public interface TokenProvider {
 
+  String NAKADI_CONFIG_WRITE = "nakadi.config.write";
+  String NAKADI_EVENT_TYPE_WRITE = "nakadi.event_type.write";
+  String NAKADI_EVENT_STREAM_WRITE = "nakadi.event_stream.write";
+  String NAKADI_EVENT_STREAM_READ = "nakadi.event_stream.read";
+  String UID = "uid";
+
   /**
    * @return a value suitable for use in an Authorization header, or null to suppress the
    * Authorization header being set
    */
-  Optional<String> authHeaderValue();
+  Optional<String> authHeaderValue(String scope);
 
 }
