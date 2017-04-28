c8ab4e802f8a24ed92ec31246f760d87b0f87ff7
dehora <bill@dehora.net>
2017-04-28 11:32:20 +0100

    Adds a marker interface for candidate API classes and methods.

---
+++ nakadi-java-client/src/main/java/nakadi/Unstable.java
@@ -0,0 +1,9 @@
+package nakadi;
+
+/**
+ * Annotates a class element or method that is a public API candidate but not yet stable and
+ * might be withdrawn.
+ *
+ */
+public @interface Unstable {
+}
