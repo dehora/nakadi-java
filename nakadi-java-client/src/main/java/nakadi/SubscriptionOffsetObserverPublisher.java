1682851bace924a0eb86a2860c9cb43488ea91e1
dehora <bill@dehora.net>
2017-04-28 13:43:58 +0100

    Adds a subscription offset observer that can be subscribed to.

---
+++ nakadi-java-client/src/main/java/nakadi/SubscriptionOffsetObserverPublisher.java
@@ -0,0 +1,76 @@
+package nakadi;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.reactivex.Flowable;
+import io.reactivex.processors.PublishProcessor;
+import io.reactivex.schedulers.Schedulers;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Unstable
+public class SubscriptionOffsetObserverPublisher implements StreamOffsetObserver {
+  private static final Logger logger = LoggerFactory.getLogger(NakadiClient.class.getSimpleName());
+
+  private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor(
+      new ThreadFactoryBuilder()
+          .setUncaughtExceptionHandler(
+              (t, e) -> logger.error("stream_observer_err {}, {}", t, e.getMessage(), e))
+          .setNameFormat("nakadi-java-stream-observer-%d").build());
+
+  private final PublishProcessor<StreamCursorContext> processor;
+  private final Flowable<StreamCursorContext> flowable;
+
+  private SubscriptionOffsetObserverPublisher() {
+    processor = PublishProcessor.create();
+    flowable = Flowable
+        .fromPublisher(processor)
+        .observeOn(Schedulers.from(EXECUTOR));
+  }
+
+  @Override public void onNext(StreamCursorContext streamCursorContext) throws NakadiException {
+    processor.onNext(streamCursorContext);
+  }
+
+  static SubscriptionOffsetObserverPublisher create() {
+    return new SubscriptionOffsetObserverPublisher();
+  }
+
+  SubscriptionOffsetObserverPublisher subscribe(StreamOffsetObserver streamOffsetObserver) {
+    flowable.subscribe(new StreamCursorContextSubscriber(streamOffsetObserver));
+    return this;
+  }
+
+  void onComplete() {
+    processor.onComplete();
+  }
+
+  class StreamCursorContextSubscriber implements Subscriber<StreamCursorContext> {
+
+    final StreamOffsetObserver streamOffsetObserver;
+
+    StreamCursorContextSubscriber(StreamOffsetObserver streamOffsetObserver) {
+      this.streamOffsetObserver = streamOffsetObserver;
+    }
+
+    @Override public void onSubscribe(Subscription s) {
+      s.request(Long.MAX_VALUE);
+    }
+
+    @Override public void onNext(StreamCursorContext streamCursorContext) {
+      streamOffsetObserver.onNext(streamCursorContext);
+    }
+
+    @Override public void onError(Throwable t) {
+      logger.error("SubscriptionOffsetObserverPublisher.subscriber onError "+t.getMessage(), t);
+    }
+
+    @Override public void onComplete() {
+      logger.info("SubscriptionOffsetObserverPublisher.subscriber onComplete ");
+    }
+  }
+
+}
