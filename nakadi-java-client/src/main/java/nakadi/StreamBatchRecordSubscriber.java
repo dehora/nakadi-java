aba30fd69289f909add11eb37b1683f5f205cf96
dehora <bill@dehora.net>
2017-05-16 03:06:46 +0100

    Handles and cleans up after observer exceptions.

--- nakadi-java-client/src/main/java/nakadi/StreamBatchRecordSubscriber.java
+++ nakadi-java-client/src/main/java/nakadi/StreamBatchRecordSubscriber.java
@@ -1,5 +1,8 @@
 package nakadi;
 
+import io.reactivex.exceptions.CompositeException;
+import io.reactivex.exceptions.Exceptions;
+import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.subscribers.ResourceSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,7 @@ class StreamBatchRecordSubscriber<T> extends ResourceSubscriber<StreamBatchRecor
 
   private final StreamObserver<T> observer;
   private final MetricCollector metricCollector;
+  private boolean done;
 
   StreamBatchRecordSubscriber(StreamObserver<T> observer, MetricCollector metricCollector) {
     super();
@@ -28,23 +32,83 @@ class StreamBatchRecordSubscriber<T> extends ResourceSubscriber<StreamBatchRecor
   }
 
   @Override public void onError(Throwable e) {
-    e.printStackTrace();
     logger.info("StreamBatchRecordSubscriber.onError " + e.getMessage());
-    observer.onError(e);
+
+    if (done) {
+      RxJavaPlugins.onError(e);
+      return;
+    }
+
+    done = true;
+
+    try {
+      observer.onError(e);
+    } catch (Exception e1) {
+      throwOnFatal(e1);
+      logger.error(e1.getMessage(), e1);
+      RxJavaPlugins.onError(new CompositeException(e, e1));
+    }
   }
 
   @Override public void onNext(StreamBatchRecord<T> record) {
-    logger.debug("StreamBatchRecordSubscriber.onNext");
-    if (!record.streamBatch().isEmpty()) {
-      metricCollector.mark(MetricCollector.Meter.receivedBatch, 1);
-      metricCollector.mark(MetricCollector.Meter.received, record.streamBatch().events().size());
-    } else {
-      metricCollector.mark(MetricCollector.Meter.receivedKeepalive, 1);
+
+    if (done) {
+      return;
     }
-    observer.onNext(record);
-    // allow the observer to set back pressure by requesting a number of items
-    observer.requestBackPressure().ifPresent(this::request);
+
+    if (record == null) {
+      Throwable npe = new NullPointerException("onNext called with null batch record. "
+          + "Null values are not expected from stream processors.");
+
+      try {
+        dispose();
+      } catch (Throwable t1) {
+        throwOnFatal(t1);
+        logger.error(t1.getMessage(), t1);
+        onError(npe);
+        return;
+      }
+      onError(npe);
+      return;
+    }
+
+    try {
+      logger.debug("StreamBatchRecordSubscriber.onNext");
+      if (!record.streamBatch().isEmpty()) {
+        metricCollector.mark(MetricCollector.Meter.receivedBatch, 1);
+        metricCollector.mark(MetricCollector.Meter.received, record.streamBatch().events().size());
+      } else {
+        metricCollector.mark(MetricCollector.Meter.receivedKeepalive, 1);
+      }
+      observer.onNext(record);
+      // allow the observer to set back pressure by requesting a number of items
+      observer.requestBackPressure().ifPresent(this::request);
+    } catch (RetryableException e) {
+      /*
+       the observer's telling us to keep going
+        */
+      logger.warn("observer_retryable_exception msg=" + e.getMessage(), e);
+    } catch (Throwable t) {
+      logger.warn("observer_non_retryable_exception msg=" + t.getMessage(), t);
+      throwOnFatal(t);
+
+      try {
+        dispose();
+      } catch (Throwable t1) {
+        throwOnFatal(t1);
+        logger.error(t1.getMessage(), t1);
+        onError(t);
+        return;
+      }
+
+      onError(t);
+    }
   }
 
-
+  private void throwOnFatal(Throwable t) {
+    Exceptions.throwIfFatal(t);
+    if (t instanceof Error) {
+      throw (Error) t;
+    }
+  }
 }
