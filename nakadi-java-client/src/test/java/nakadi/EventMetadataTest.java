bdf0d18c54f6cf2b907bcd2b09d9a61ff9458164
dehora <bill@dehora.net>
2018-11-08 14:19:06 +0000

    Adds span tracing to event metadata.
    For #336.

--- nakadi-java-client/src/test/java/nakadi/EventMetadataTest.java
+++ nakadi-java-client/src/test/java/nakadi/EventMetadataTest.java
@@ -1,10 +1,26 @@
 package nakadi;
 
+import com.google.common.collect.Maps;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class EventMetadataTest {
+
+  @Test
+  public void testSpan() {
+    final EventMetadata eventMetadata = EventMetadata.newPreparedEventMetadata();
+    assertNull(eventMetadata.spanCtx());
+    final Map<String, String> ctx = Maps.newHashMap();
+    ctx.put("ot-tracer-spanid", "42");
+    ctx.put("ot-tracer-traceid", "e9435c17dabe8238");
+    ctx.put("ot-baggage-foo", "bar");
+
+    eventMetadata.spanCtx(ctx);
+    assertEquals(ctx, eventMetadata.spanCtx());
+  }
 
   @Test
   public void testNewPreparedEventMetadata() {
