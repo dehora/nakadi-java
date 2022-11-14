a633e724d29ae8dc166ffca8653e231bcd403fb3
dehora <bill@dehora.net>
2022-11-14 14:37:25 +0000

    Adds tests to verify behaviour added in #379.

---
+++ nakadi-java-client/src/test/java/nakadi/ResourceSupportTest.java
@@ -0,0 +1,37 @@
+package nakadi;
+
+import junit.framework.TestCase;
+
+public class ResourceSupportTest extends TestCase {
+
+  public void testOptions() {
+
+    String accept = "application/bar";
+    ResourceOptions options = ResourceSupport.options(accept);
+
+    assertEquals(options.headers().get(ResourceOptions.HEADER_ACCEPT), accept);
+
+    assertEquals(options.headers().get(ResourceOptions.HEADER_ACCEPT_CHARSET),
+        "UTF-8");
+
+    assertNull(options.headers().get(ResourceOptions.HEADER_CONTENT_TYPE));
+  }
+
+  public void testOptionsWithJsonContent() {
+
+    String accept = "application/foo";
+    ResourceOptions options = ResourceSupport.options(accept);
+
+    assertEquals(options.headers().get(ResourceOptions.HEADER_ACCEPT), accept);
+
+    assertEquals(options.headers().get(ResourceOptions.HEADER_ACCEPT_CHARSET),
+        "UTF-8");
+
+    assertNull(options.headers().get(ResourceOptions.HEADER_CONTENT_TYPE));
+
+    ResourceOptions options1 = ResourceSupport.optionsWithJsonContent(options);
+
+    assertEquals(options1.headers().get(ResourceOptions.HEADER_CONTENT_TYPE),
+        "application/json; charset=utf8");
+  }
+}
