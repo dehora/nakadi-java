0bec16983499d5656b851e13a9a1bf4b9215755b
dehora <bill@dehora.net>
2023-10-24 11:47:45 +0100

    Adds an avro module.
    This provides a module skeleton for an avro extension to the
    client. The avro files for envelope and batch publishing along
    with some exceptions are here to bootstrap the gradle build.

---
+++ nakadi-java-avro/src/test/java/nakadi/avro/AvroPublishingBatchSerializerTest.java
@@ -0,0 +1,14 @@
+package nakadi.avro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AvroPublishingBatchSerializerTest {
+
+  @Test
+  public void test() {
+    assertEquals("a", "a");
+  }
+
+}
